5ff577c1e6caaf423074e790d509a6af1e6b4e23
khacquyet dang home <khacuyet.dang.pro@gmail.com>
2017-04-13 19:57:39 +0200

    init the project need to implement the last search artist and show his top track in the coookie

---
+++ src/Gallery.jsx
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Gallery extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      playingUrl : '',
+      audio: null,
+      playing: false
+    }
+  }
+  playAudio(previewUrl)
+  {
+    let audio = new Audio(previewUrl);
+    if (this.state.playing === false) {
+      audio.play();
+      this.setState({playing: true, playingUrl: previewUrl, audio});
+    } else {
+      if (this.state.playingUrl === previewUrl) {
+        this.state.audio.pause();
+        this.setState({playing: false});
+      } else {
+        this.state.audio.pause();
+        audio.play();
+        this.setState({playing: true, playingUrl: previewUrl, audio});
+      }
+    }
+  }
+  render() {
+    const {tracks} = this.props;
+    return (
+      <div>
+        {tracks != null
+          ? (tracks.map((track, k) => {
+            console.log("track: ", track);
+            const tracksImg = track.album.images[0].url;
+            return <div key={k} className="track" onClick={() => this.playAudio(track.preview_url)}>
+              <img src={tracksImg} className="track-img" alt="track"></img>
+              <div className="track-play">
+                <div className="track-play-inner">
+                  {
+                    this.state.playingUrl === track.preview_url ?
+                    <span>| |</span>
+                    : <span>&#9654;</span>
+                  }
+
+                </div>
+              </div>
+              <p className="track-text">
+                {track.name}
+              </p>
+            </div>;
+          }))
+          : <div></div>}
+        </div>
+      );
+    }
+
+  }
+
+  export default Gallery;
